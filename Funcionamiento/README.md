de29e21a464c183d7e380c154f7d319915c45ebf
jaimb99 <jaimemartin.19@campuscamara.es>
2022-12-14 19:45:33 +0100

    Cliente y servidor junto con construccion excel

---
+++ Funcionamiento/README.md
@@ -0,0 +1,4 @@
+# APPGastos
+Esta App consiste en que ingresas en el telefono los gastos.
+Estos se envían a un server, donde se alojan y se añaden a un excell configurado (nombre, cantidad,fecha)
+Cuando quieras, puedes solicitar la descarga de dicho excell desde el movil.
---
+++ Funcionamiento/cliente.py
@@ -0,0 +1,39 @@
+from asyncore import read
+from socket import socket,error
+from pathlib import Path
+def main():
+    s=socket()
+    s.connect(("localhost",6030))
+
+    print("Conexion con puerto 6030")
+    file_size = Path(r'example.xls').stat().st_size
+    while True:
+        f=open("example.xls","rb")
+        tam=(str)(file_size)
+        print(tam)
+        print(file_size)
+        content=f.read(file_size)
+    
+        while content:
+            #enviar tamaño 
+            s.send((bytes)(tam,'ascii'))
+            #Enviar archivo
+            s.send(content)
+            content=f.read(file_size)
+            
+        break
+    #~Se utiliza el 1 para inidicar al cliente que ya se ha mandado todo.
+    try:
+        s.send(chr(1))
+    except TypeError:
+        s.send(bytes(chr(1), "utf-8"))
+    
+    #se cierran conexiones
+    s.close()
+    f.close()
+    print("El archivo se envió correnctamente")
+
+
+if __name__ == "__main__":
+    main()
+
